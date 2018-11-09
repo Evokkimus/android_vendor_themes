4f33b9822e167d8df8dccba919fb396f7fb918d8
timocapa <timo.capa@gmail.com>
2018-11-09 06:19:39 +0100

    Revert "New Boot animation"
    This reverts commit 7dbd8ae1b5e8228ba997df98fb3da3222c329868.

--- common.mk
+++ common.mk
@@ -1,14 +1,11 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 
-# Accents
-include vendor/themes/accents.mk
-
-# Boot animation
-include vendor/themes/bootanimation.mk
-
 # Sounds
 include vendor/themes/sounds.mk
+
+# Accents
+include vendor/themes/accents.mk
 
 # Themes
 include vendor/themes/themes.mk
